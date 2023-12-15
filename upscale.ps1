f7b3f75d0367788a4f52c33e05f3fa3db2f29ca0
dajk1 <104295556+dajk1@users.noreply.github.com>
2023-12-15 20:57:27 +0100

    hh

--- upscale.ps1
+++ upscale.ps1
@@ -1,8 +1,5 @@
-Set-AzAppServicePlan -NumberofWorkers 1 -Name ASP-DAJKSQL-871a -Tier Basic -ResourceGroupName DAJK_SQL -WorkerSize Small
-#jkhadskjhaskjdhdsa
-Get-AzVM | `
-  Select-Object Name, @{Name="Status";
-        Expression={(Get-AzVM -Name $_.Name -ResourceGroupName $_.ResourceGroupName -status).Statuses[1].displayStatus}}
+# 1. Working with the environment
+
+$env:PSModulePath -split "(:)"
 
 
-        Get-AzVm -ResourceGroupName dajk_rg | Select-Object Name, PowerState
